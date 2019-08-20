14640162766531ec288ba6166a901de30be8557a
adam116 <54105639+adam116@users.noreply.github.com>
2019-08-20 19:39:12 +0530

    Update Pynkynmaw_Q5.c

--- Week 1/09/Pynkynmaw_Q5.c
+++ Week 1/09/Pynkynmaw_Q5.c
@@ -1,3 +1,7 @@
+//Write a program to input the roll no., marks in 3 subjects and marks in one additional subject for N number of students. Compute the total marks, excess additional marks should be added to the total marks and determine the division. No division should be given if fail in 1 or more subjects.
+
+
+
 #include<stdio.h>
 #include<conio.h>
 
